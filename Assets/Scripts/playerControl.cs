1fe745a597496554acacb3ef27dcddae5cc51a4d
ganjaged <ganjaged@gmail.com>
2014-06-03 21:56:59 +0200

    Added player to networking
    Added player control
    Added networking between players

---
+++ Assets/Scripts/playerControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour {
+
+    public float speed = 5;
+
+    void Start()
+    {
+        if (!networkView.isMine)
+        {
+            enabled = false;
+        }
+    }
+
+    void Update() 
+    {
+        if (networkView.isMine) 
+        {
+            Vector3 moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            transform.Translate(speed * moveDir * Time.deltaTime);
+        }
+    }
+
+    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            Vector3 pos = transform.position;
+            stream.Serialize(ref pos);
+        }
+        else
+        {
+            Vector3 posRec = Vector3.zero;
+            stream.Serialize(ref posRec);
+            transform.position = posRec;
+        }
+    }
+}
---
+++ Assets/Scripts/spawnScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnScript : MonoBehaviour {
+
+    public Transform player;
+
+    void OnServerInitialized() 
+    {
+        SpawnPlayer();
+    }
+
+    void OnConnectedToServer() 
+    {
+        SpawnPlayer();
+    }
+    
+    void SpawnPlayer()
+    {
+        Network.Instantiate(player, transform.position, transform.rotation, 0);
+    }
+   
+    void OnPlayerDisconnected(NetworkPlayer player)
+    {
+        Network.RemoveRPCs(player);
+        Network.DestroyPlayerObjects(player);
+    }
+
+    void OnDisconnectedFromServer(NetworkDisconnection info)
+    {
+        Network.RemoveRPCs(Network.player);
+        Network.DestroyPlayerObjects(Network.player);
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    
+
+}
