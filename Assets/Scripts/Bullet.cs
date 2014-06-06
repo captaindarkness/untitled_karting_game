368a4481b961be77e623e51519a9945d34e5044d
captaindarkness <jonathanstrandbech@gmail.com>
2014-06-06 16:00:04 +0200

    Added bullet spawner
    -Added bullet prefab
    -Added scripts: bulletForce, bulletspawner (use space to fire)
    (This can be used for shooting stuff from the kart aka Rockets)

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public Transform bullet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Instantiate(bullet, transform.position, transform.rotation);
+		}
+
+	}
+}
---
+++ Assets/Scripts/BulletForce.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletForce : MonoBehaviour {
+
+	public float power;
+	public float lifeSpan;
+
+	// Use this for initialization
+	void Start () {
+		power = 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0, 0, power * Time.deltaTime);
+
+		Destroy(gameObject, lifeSpan);
+	}
+}
